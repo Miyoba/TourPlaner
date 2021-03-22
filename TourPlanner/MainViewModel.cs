4f6bf22cf09e57e6fea9b703a6fa73d9fe0de0cc
Wolfgang <Mair>
2021-03-22 02:54:39 +0100

    MainWindow xaml prepared, first Models created, images added

---
+++ TourPlanner/MainViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using TourPlanner.Model;
+
+namespace TourPlanner
+{
+    class MainViewModel: INotifyPropertyChanged
+    {
+
+        public ObservableCollection<Tour> Data { get; }
+            = new ObservableCollection<Tour>();
+
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string RouteInformation { get; set; }
+        public int Distance { get; set; }
+        public RelayCommand AddCommand { get; }
+
+        public MainViewModel()
+        {
+            AddCommand = new RelayCommand((_) =>
+            {
+                Data.Add(new Tour(this.Name, this.Description, this.RouteInformation, this.Distance));
+                Name = String.Empty;
+                Description = String.Empty;
+                RouteInformation = String.Empty;
+                Distance = 0;
+                OnPropertyChanged(nameof(Name));
+                OnPropertyChanged(nameof(Description));
+                OnPropertyChanged(nameof(RouteInformation));
+                OnPropertyChanged(nameof(Distance));
+            });
+
+            // real data to add (not design data)
+            Data.Add(new Tour("TestTour","TestTourDescription","TestTourInfo", 0));
+            Data.Add(new Tour("TestTour2","TestTourDescription2","TestTourInfo2", 2));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ TourPlanner/Model/TourLog.cs
@@ -0,0 +1,79 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+
+namespace TourPlanner.Model
+{
+    public class TourLog: INotifyPropertyChanged
+    {
+        private string _time;
+        private string _report;
+        private int _distance;
+        private string _totalTime;
+        private double _rating;
+        public TourLog(string time, string report, int distance, string totalTime, double rating)
+        {
+            _time = time;
+            _report = report;
+            _distance = distance;
+            _totalTime = totalTime;
+            _rating = rating;
+        }
+
+        public string Time
+        {
+            get => _time;
+            set
+            {
+                _time = value;
+                OnPropertyChanged(); // using CallerMemberName
+            }
+        }
+
+        public string Report
+        {
+            get => _report;
+            set
+            {
+                _report = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public int Distance
+        {
+            get => _distance;
+            set
+            {
+                _distance = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string TotalTime
+        {
+            get => _totalTime;
+            set
+            {
+                _totalTime = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public double Rating
+        {
+            get => _rating;
+            set
+            {
+                _rating = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
