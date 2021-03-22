3f0033434f1a2c0bfe190536efbc4ecc54cf7a8d
Wolfgang <Mair>
2021-03-22 10:58:48 +0100

    Small Presentation changes and DB class creation

---
+++ TourPlanner/DataAccessLayer/DbConnection.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TourPlanner.Model;
+
+namespace TourPlanner.DataAccessLayer
+{
+    class DbConnection:IDataAccess
+    {
+        public List<Tour> GetTours()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TourPlanner/DataAccessLayer/IDataAccess.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TourPlanner.Model;
+
+namespace TourPlanner.DataAccessLayer
+{
+    interface IDataAccess
+    {
+        public List<Tour> GetTours();
+    }
+}
