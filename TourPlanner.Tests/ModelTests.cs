fe6c4dcd0e901ffeec19b5a0b2d9e247d97262d4
Wolfgang Mair <wolfgang_mair@hotmail.com>
2021-06-02 01:31:51 +0200

    Created first Model Unit Tests

---
+++ TourPlanner.Tests/ModelTests.cs
@@ -0,0 +1,48 @@
+﻿using NUnit.Framework;
+using TourPlanner.Models;
+
+namespace TourPlanner.Tests {
+    public class ModelTests {
+        [Test]
+        public void TourHasImageTest_TourHasNullObjectAsPath_ShouldReturnFalse()
+        {
+            //arrange
+            Tour tour = new Tour(1, "Name", "Description", "Wien", "Berlin", 123);
+            //act
+            tour.ImagePath = null;
+            //assert
+            Assert.False(tour.HasImage());
+        }
+
+        [Test]
+        public void TourHasImageTest_TourHasEmptyStringAsPath_ShouldReturnFalse()
+        {
+            //arrange
+            Tour tour = new Tour(1, "Name", "Description", "Wien", "Berlin", 123);
+            //act
+            tour.ImagePath = "";
+            //assert
+            Assert.False(tour.HasImage());
+        }
+
+        [Test]
+        public void TourHasImageTest_TourHasPathLeadingToNoFile_ShouldReturnFalse()
+        {
+            //arrange
+            Tour tour = new Tour(1, "Name", "Description", "Wien", "Berlin", 123);
+            //act
+            tour.ImagePath = @"C:\";
+            //assert
+            Assert.False(tour.HasImage());
+        }
+        [Test]
+        public void TourHasImageTest_TourHasImagePath_ShouldReturnTrue()
+        {
+            //arrange
+            Tour tour = new Tour(1, "Name", "Description", "Wien", "Berlin", 123,@"..\..\..\..\TourPlanner\Images\Icon\Search_Icon.png");
+            //act
+            //assert
+            Assert.True(tour.HasImage());
+        }
+    }
+}
